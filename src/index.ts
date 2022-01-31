5d621f036a2a745be9ffc8a01394252fda60e8a7
jsl0319 <kokonb0@gmail.com>
2022-02-01 00:32:58 +0900

    classes on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-interface Human {
-  name:string;
-  age:number;
-  gender:string;
+class Human {
+  public name:string;
+  public age:number;
+  public gender:string;
+  constructor(name:string, age:number, gender:string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name : 'zsun',
-  age : 20,
-  gender: 'F'
-}
+const person = new Human('zsun',22,'female');
 
 const printInfo = (person:Human) : void => {
   console.log(`저는 ${person.name}이고 나이는 ${person.age}살이며 ${person.gender}입니다.`)
