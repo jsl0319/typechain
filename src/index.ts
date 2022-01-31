4b257eab4da4bd8564f5ec3b5550b204b941617f
jsl0319 <kokonb0@gmail.com>
2022-02-01 00:47:12 +0900

    BlockChain creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-class Human {
-  public name:string;
-  public age:number;
-  public gender:string;
-  constructor(name:string, age:number, gender:string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
-  }
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number) {
+      this.index = index;
+      this.hash = hash;
+      this.previousHash = previousHash;
+      this.data = data;
+      this.timestamp = timestamp;
+    }
 }
 
-const person = new Human('zsun',22,'female');
+const genesisBlock:Block = new Block(1, 'hash', 'preHash', '', 2);
 
-const printInfo = (person:Human) : void => {
-  console.log(`저는 ${person.name}이고 나이는 ${person.age}살이며 ${person.gender}입니다.`)
-}
-
-printInfo(person);
+let blockChain:[Block] = [genesisBlock];
+console.log(blockChain);
