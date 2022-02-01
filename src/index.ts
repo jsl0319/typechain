87680c58d660044c76039a3dd62c85bb6d056dc9
jsl0319 <kokonb0@gmail.com>
2022-02-01 22:42:23 +0900

    add Block

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ class Block {
     previousHash:string):string => CryptoJS.SHA256(index + data + timestamp + previousHash).toString();
 
   // Block 구조 유효성 검사
-  static validateStrucure = (aBlock:Block) : boolean => {
+  static validateStructure = (aBlock:Block) : boolean => {
     if(typeof aBlock.index === "number" && 
     typeof aBlock.data === "string" && 
     typeof aBlock.previousHash === "string" &&
@@ -42,30 +42,45 @@ class Block {
 const genesisBlock:Block = new Block(0, 'hash', 'preHash', 'data', 123456);
 let blockChain:Block[] = [genesisBlock];
 
-const getBlockchain:Block[] = blockChain;
-const getLatestBlock: Block = getBlockchain[getBlockchain.length - 1];
+const getLatestBlock = () : Block => blockChain[blockChain.length - 1];
 const getNewTimestamp: number = Math.round(new Date().getTime() / 1000);
+const getHashforBlock = (aBlock:Block) : string => Block.calculateBlockHash(aBlock.index,aBlock.data,aBlock.timestamp,aBlock.previousHash);
 
 // create next block
 const createNewBlock = (data:string) : Block => {
-  const previousBlock:Block = getLatestBlock;
+  const previousBlock:Block = getLatestBlock();
   const newIndex:number = previousBlock.index + 1;
   const previousHash:string = previousBlock.hash;
   const newTimestamp:number = getNewTimestamp;
   const newHash:string = Block.calculateBlockHash(newIndex, data, newTimestamp, previousHash);
 
   const newBlock:Block = new Block(newIndex, newHash, previousHash, data, newTimestamp);
+  addBlock(newBlock);
+
   return newBlock;
 }
 
 // validate new block
 const isBlockValid = (candidateBlock:Block, previousBlock:Block) : boolean => {
-if(Block.validateStrucure(candidateBlock) ||
-  candidateBlock.previousHash !== previousBlock.hash || 
-  candidateBlock.index !== previousBlock.index + 1
-  ) return false;
-  return true;
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false;
+    } else {
+    return true;
+    }
 }
 
-console.log(createNewBlock('1234fff'));
-
+// add Block
+const addBlock = (aBlock:Block) : void => {
+  if(isBlockValid(aBlock, getLatestBlock()))
+      blockChain.push(aBlock)
+}
+createNewBlock('second block');
+createNewBlock('third block');
+createNewBlock('fourth block');
+console.log(blockChain);
